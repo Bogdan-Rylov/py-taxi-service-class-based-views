a699b98d72bb87d08d25be934d29c73332b16eb2
Bogdan <bogdan.rylov422@gmail.com>
2024-09-11 00:00:47 +0300

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -1,9 +1,14 @@
+from http.client import HTTPResponse
+
+from django.db.models import Prefetch
+from django.http import HttpRequest
 from django.shortcuts import render
+from django.views import generic
 
 from taxi.models import Driver, Car, Manufacturer
 
 
-def index(request):
+def index(request: HttpRequest) -> HTTPResponse:
     """View function for the home page of the site."""
 
     context = {
@@ -13,3 +18,39 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(generic.ListView):
+    model = Manufacturer
+    template_name = "taxi/manufacturer_list.html"
+    context_object_name = "manufacturer_list"
+    paginate_by = 5
+
+
+class CarListView(generic.ListView):
+    model = Car
+    template_name = "taxi/car_list.html"
+    context_object_name = "car_list"
+    queryset = Car.objects.all().select_related("manufacturer")
+    paginate_by = 5
+
+
+class CarDetailView(generic.DetailView):
+    model = Car
+
+
+class DriverListView(generic.ListView):
+    model = Driver
+    template_name = "taxi/driver_list.html"
+    context_object_name = "driver_list"
+    paginate_by = 5
+
+
+class DriverDetailView(generic.DetailView):
+    model = Driver
+    queryset = Driver.objects.prefetch_related(
+        Prefetch(
+            "cars",
+            queryset=Car.objects.select_related("manufacturer")
+        )
+    )
